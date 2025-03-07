e3f0001bef83667ac5cbb458083010bed8a18cb3
shadythegiant <hellgatesdesigner2010@gmail.com>
2025-03-07 20:35:51 +0000

    trying routes for the firstime  :octocat:

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+
+//
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Hello there </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="product" element={<Product />}></Route>
+          <Route path="pricing" element={<Pricing />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
