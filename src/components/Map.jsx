dd9b35f926d16ee3fdd8c908c48ca937a7bad9fa
shadythegiant <hellgatesdesigner2010@gmail.com>
2025-03-11 22:17:51 +0000

    leaflet marker issue

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,13 +2,14 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
 import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 export default function Map() {
   // use navigate hook
 
   const navigate = useNavigate();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-
+  const { cities } = useCities();
   //
 
   //  storing state in URL and exploring hook in react query
@@ -17,16 +18,16 @@ export default function Map() {
   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+    <div className={styles.mapContainer}>
       <MapContainer
         center={mapPosition}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
         className={styles.map}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
         <Marker position={mapPosition}>
           <Popup>
